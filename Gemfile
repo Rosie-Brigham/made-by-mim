79abcdc24d665940f07d614a3d7af0b2954d8ff1
Rosie-Brigham <rosie@woolandthegang.com>
2015-04-16 23:11:08 +0100

    getting there...

--- Gemfile
+++ Gemfile
@@ -23,7 +23,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+#images
 gem "paperclip", "~> 4.2"
+gem 'aws-sdk', '~> 1.5.7'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
-  has_attached_file :image, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image,
+    :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" },
+    :default_url => "/images/:style/missing.png",
+    :storage => :s3,
+    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
