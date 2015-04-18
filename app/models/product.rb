41305aeca7d5bab8ff2d839a264821436e33abb0
Rosie-Brigham <rosie@woolandthegang.com>
2015-04-18 09:24:04 +0100

    gets s3 working, hopefully

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
     :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" },
     :default_url => "/images/:style/missing.png",
     :storage => :s3,
-    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+    :s3_credentials => "#{Rails.root}/config/s3.yml"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
