8677221b103820d8ccbd71ee128e684b4cdbaff7
Rosie-Brigham <rosie@woolandthegang.com>
2015-04-23 21:28:20 +0100

    images working???

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ActiveRecord::Base
     :large => "600x600>",
     :medium => "300x300>",
     :thumb => "100x100>"
-  },
-    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-    :url => "/system/:attachment/:id/:style/:filename"
+  }
+    # :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    # :url => "/system/:attachment/:id/:style/:filename"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
